8cf9c968de4aeaca31cadc954e890737d1a071ff
Casandra <casandragrullon@pursuit.org>
2020-04-14 22:41:30 -0400

    created activity model and database session to get back firebase data

---
+++ ActivityApp/FirebaseServices/DatabaseService.swift
@@ -0,0 +1,28 @@
+//
+//  DatabaseService.swift
+//  ActivityApp
+//
+//  Created by casandra grullon on 4/14/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+
+private let db = Firestore.firestore()
+class DatabaseService {
+    
+    private init() {}
+    public static let shared = DatabaseService()
+    
+    public func getActivities(id: String,completion: @escaping (Result<[Activity], Error>) -> ()) {
+        db.collection("activities").getDocuments { (snapshot, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let snapshot = snapshot {
+                let activities = snapshot.documents.compactMap {Activity ($0.data())}
+                completion(.success(activities))
+            }
+        }
+    }
+}
