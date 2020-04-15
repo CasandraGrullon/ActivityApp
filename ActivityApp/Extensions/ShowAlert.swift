963663a9809edade11f9af280ff272791aaa66ab
Casandra <casandragrullon@pursuit.org>
2020-04-14 22:45:12 -0400

    added url extension and show alert

---
+++ ActivityApp/Extensions/ShowAlert.swift
@@ -0,0 +1,18 @@
+//
+//  ShowAlert.swift
+//  ActivityApp
+//
+//  Created by casandra grullon on 4/14/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  func showAlert(title: String, message: String, completion: ((UIAlertAction) -> Void)? = nil) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "Ok", style: .default, handler: completion)
+    alertController.addAction(okAction)
+    present(alertController, animated: true, completion: nil)
+  }
+}
