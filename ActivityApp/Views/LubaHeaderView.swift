8656280fcd941b1529ca636c50974c4ef3e71334
Casandra <casandragrullon@pursuit.org>
2020-04-16 11:28:48 -0400

    added header view

---
+++ ActivityApp/Views/LubaHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  LubaHeaderView.swift
+//  ActivityApp
+//
+//  Created by casandra grullon on 4/16/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class LubaHeaderView: UICollectionReusableView {
+        
+}
