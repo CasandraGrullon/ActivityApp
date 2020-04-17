154d9ff6a02788636801c4fce719bd66af218175
Casandra <casandragrullon@pursuit.org>
2020-04-16 21:44:02 -0400

    added interview activity cell, vc and view

--- ActivityApp/Views/CasandraView.swift
+++ ActivityApp/Views/CasandraView.swift
@@ -9,7 +9,101 @@
 import UIKit
 
 class CasandraView: UIView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        questionsLabel.clipsToBounds = true
+        questionsLabel.layer.cornerRadius = 13
+        cameraButton.clipsToBounds = true
+        cameraButton.layer.cornerRadius = 13
+        galleryButton.clipsToBounds = true
+        galleryButton.layer.cornerRadius = 13
+        collectionView.clipsToBounds = true
+        collectionView.layer.cornerRadius = 13
+    }
+    public lazy var questionsLabel: UILabel = {
+    let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .white
+        label.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        label.numberOfLines = 0
+        label.font = UIFont(name: "Helvetica", size: 20)
+        label.text = "Interview Questions\n\n1. What/who is your biggest inspiration?\n2. What do you want to be when you grow up?\n3. What is your favorite color?\n4. What is the last movie you saw?\n"
+        return label
+        
+    }()
+    public lazy var buttonStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [cameraButton, galleryButton])
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.alignment = .fill
+        stack.spacing = 5
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    public lazy var cameraButton: UIButton = {
+       let button = UIButton()
+        button.setImage(UIImage(systemName: "video.fill"), for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        return button
+    }()
+    public lazy var galleryButton: UIButton = {
+       let button = UIButton()
+        button.setImage(UIImage(systemName: "photo.on.rectangle.fill"), for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        return button
+    }()
+    public lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    private func commonInit() {
+        configureLabelConstraints()
+        configureButtonStack()
+        configureCollectionViewConstraints()
+    }
 
-
-
+    private func configureLabelConstraints() {
+        addSubview(questionsLabel)
+        questionsLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            questionsLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            questionsLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            questionsLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20)
+        ])
+    }
+    private func configureButtonStack() {
+        addSubview(buttonStack)
+        buttonStack.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            buttonStack.topAnchor.constraint(equalTo: questionsLabel.bottomAnchor, constant: 20),
+            buttonStack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            buttonStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            buttonStack.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    private func configureCollectionViewConstraints() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: buttonStack.bottomAnchor, constant: 10),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+    }
+    
 }
+
